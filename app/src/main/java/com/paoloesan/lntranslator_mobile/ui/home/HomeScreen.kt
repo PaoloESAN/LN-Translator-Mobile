a3eab80927746ff2a9fb08119090fb8f336c7d38
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-01-17 09:48:09 -0500

    feat: pantalla principal

---
+++ app/src/main/java/com/paoloesan/lntranslator_mobile/ui/home/HomeScreen.kt
@@ -0,0 +1,112 @@
+package com.paoloesan.lntranslator_mobile.ui.home
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val prompt = remember { mutableStateOf("") }
+    fun setPrompt(newPrompt: String) {
+        prompt.value = newPrompt
+    }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+        ) {
+
+            Image(
+                painter = androidx.compose.ui.res.painterResource(id = com.paoloesan.lntranslator_mobile.R.drawable.ln_translator_logo),
+                contentDescription = "LN Translator Logo",
+                modifier = Modifier
+                    .size(200.dp)
+                    .aspectRatio(1f)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = "\"Ingresa un prompt y empieza a traducir.\"",
+                fontStyle = FontStyle.Italic
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    label = { Text(text = "Prompt de contexto") },
+                    value = prompt.value,
+                    onValueChange = { setPrompt(it) },
+                    minLines = 3,
+                    modifier = Modifier.weight(1f)
+                )
+                Column {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ChatBubbleOutline,
+                            contentDescription = "Ver prompts guardados"
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Save,
+                            contentDescription = "Guardar prompt actual"
+                        )
+                    }
+                }
+            }
+
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Iniciando traduccion", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Iniciar")
+            }
+        }
+    }
+}
