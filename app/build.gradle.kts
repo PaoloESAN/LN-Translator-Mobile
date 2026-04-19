aa9c8f17c0716d0f4f74d77368b6c403c3ec2f7f
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-04-19 11:12:47 -0500

    chore: version 1.3.0 en build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.paoloesan.lntranslator_mobile"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
